2f2fa87109f13590b21e9bc4412fa93dccb4f0b7
Joel Benner <jbenner12@gmail.com>
2015-04-27 00:01:51 -0700

    disgard

---
+++ Javascript/javascript.js
@@ -0,0 +1,7 @@
+
+    
+function Button_Vanish() { 
+    document.getElementById("test").innerHTML = "Cognitopia crafts simple apps to help people manage each day's challenges. We believe that with a little help from your phone or iPad, people with autism can be more independent. Our apps also deepen the connection between caregiver and user by providing notification and support to those in the user's circle of trust.";
+    LearnHowButton.style.visibility = "hidden";
+}
+
